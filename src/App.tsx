35eab2aac4efc41415fa685a73d452b513f7ab0e
Nastassia27 <anastasia.derugo@gmail.com>
2025-02-17 17:11:07 +0300

    add all elements

--- src/App.tsx
+++ src/App.tsx
@@ -2,27 +2,51 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Input from "./components/Input/Input";
+import Button from "./components/Button/Button";
 
 function App() {
     const [startValue, setStartValue] = React.useState('');
     const [maxValue, setMaxValue] = React.useState('');
+    const [resultValue, setResultValue] = React.useState('');
 
-    const onChangeMaxValueHandler=()=>{
+    const onChangeMaxValueHandler = () => {
         setMaxValue('');
     }
 
-    const onChangeStartValueHandler=()=>{
+    const onChangeStartValueHandler = () => {
         setStartValue('');
+    }
+    const setNumbersHandler = () => {
+
+    }
+    const increaseValueHandler = () => {
+        setResultValue('')
+    }
+
+    const resetValueHandler = () => {
+        setResultValue('')
     }
     return (
         <div className="App">
-            <div>
-                Max value:
-                <Input value={maxValue} onChange={onChangeMaxValueHandler} type='number'/>
+            <div className='set-numbers'>
+                <div>
+                    Max value:
+                    <Input value={maxValue} onChange={onChangeMaxValueHandler} type='number'/>
+                </div>
+                <div>
+                    Start value:
+                    <Input value={startValue} onChange={onChangeStartValueHandler} type='number'/>
+                </div>
+                <div>
+                    <Button title={'set'} onClick={setNumbersHandler}/>
+                </div>
             </div>
-            <div>
-                Start value:
-                <Input value={startValue} onChange={onChangeStartValueHandler} type='number'/>
+            <div className='show-results'>
+                <div>{resultValue}{2}</div>
+                <>
+                    <Button title={'inc'} onClick={increaseValueHandler}/>
+                    <Button title={'reset'} onClick={resetValueHandler}/>
+                </>
             </div>
         </div>
     );
