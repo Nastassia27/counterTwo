a638a19f1cd2dd71770018750963dda9983b81d4
Nastassia27 <anastasia.derugo@gmail.com>
2025-02-17 17:02:40 +0300

    create structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,31 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Input from "./components/Input/Input";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [startValue, setStartValue] = React.useState('');
+    const [maxValue, setMaxValue] = React.useState('');
+
+    const onChangeMaxValueHandler=()=>{
+        setMaxValue('');
+    }
+
+    const onChangeStartValueHandler=()=>{
+        setStartValue('');
+    }
+    return (
+        <div className="App">
+            <div>
+                Max value:
+                <Input value={maxValue} onChange={onChangeMaxValueHandler} type='number'/>
+            </div>
+            <div>
+                Start value:
+                <Input value={startValue} onChange={onChangeStartValueHandler} type='number'/>
+            </div>
+        </div>
+    );
 }
 
 export default App;
