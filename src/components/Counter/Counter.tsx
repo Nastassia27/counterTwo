05e35ed3b5281543d7fb2e8f65faf303d66644bd
Nastassia27 <anastasia.derugo@gmail.com>
2025-02-21 13:43:51 +0300

    save values in localstorage

--- src/components/Counter/Counter.tsx
+++ src/components/Counter/Counter.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import SetValues from "../SetValues/SetValues";
 import ShowResults from "../ShowResults/ShowResults";
 import './counter.css';
@@ -11,10 +11,34 @@ const Counter = () => {
     const [maxValue, setMaxValue] = React.useState<number>(0);
     const [maxValueFix, setMaxValueFix] = React.useState<number>(0);
 
-    const [resultValue, setResultValue] = React.useState<number>(0);
+    const [resultValue, setResultValue] = React.useState<number>(() => {
+        let resultValueString = localStorage.getItem('resultValue');
+        return (resultValueString ? JSON.parse(resultValueString) : 0)
+    });
 
-    const [isClicked, setIsClicked]= React.useState<boolean>(false);
-    //const [disableButtons, setDisabledButton] = React.useState(false);
+    const [isClicked, setIsClicked] = React.useState<boolean>(false);
+
+    useEffect(() => {
+        let startValueString = localStorage.getItem('startValue');
+        startValueString ? setStartValue(JSON.parse(startValueString) as number) : setStartValue(startValue)
+
+        let maxValueString = localStorage.getItem('maxValue');
+        maxValueString ? setMaxValue(JSON.parse(maxValueString) as number) : setMaxValue(maxValue)
+
+        let isClickedString = localStorage.getItem('isClicked');
+        isClickedString ? setIsClicked(JSON.parse(isClickedString)) : setIsClicked(isClicked)
+
+        let resultValueString = localStorage.getItem('resultValue');
+        resultValueString ? setResultValue(JSON.parse(resultValueString)) : setResultValue(resultValue)
+
+    }, []);
+
+    useEffect(() => {
+        localStorage.setItem('startValue', JSON.stringify(startValue))
+        localStorage.setItem('maxValue', JSON.stringify(maxValue))
+        localStorage.setItem('resultValue', JSON.stringify(resultValue))
+        localStorage.setItem('isClicked', JSON.stringify(isClicked))
+    }, [startValue, maxValue, resultValue, isClicked])
 
 
     const onChangeMaxValueHandler = (number: number) => {
@@ -33,7 +57,7 @@ const Counter = () => {
         setIsClicked(true)
     }
     const increaseValueHandler = () => {
-        const newValue= resultValue+1
+        const newValue = resultValue + 1
         setResultValue(newValue)
     }
 
@@ -50,13 +74,13 @@ const Counter = () => {
                        isClicked={isClicked}
 
             />
-        <ShowResults resultValue={resultValue}
-                     increaseValue={increaseValueHandler}
-                     resetValue={resetValueHandler}
-                     maxValue={maxValue}
-                     startValue={startValue}
-                     isClicked={isClicked}
-        />
+            <ShowResults resultValue={resultValue}
+                         increaseValue={increaseValueHandler}
+                         resetValue={resetValueHandler}
+                         maxValue={maxValue}
+                         startValue={startValue}
+                         isClicked={isClicked}
+            />
         </div>
     );
 }
